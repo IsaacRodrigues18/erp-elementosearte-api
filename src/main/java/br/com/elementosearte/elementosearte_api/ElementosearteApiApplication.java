43ea1772ef30f54c5af1677912bf609b7d59ffdb
Isaac Rodrigues <isaacrds2002@gmail.com>
2026-04-14 11:36:25 -0300

    Inicializando o projeto

---
+++ src/main/java/br/com/elementosearte/elementosearte_api/ElementosearteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.elementosearte.elementosearte_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementosearteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementosearteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elementosearte/elementosearte_api/ElementosearteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elementosearte.elementosearte_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementosearteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
