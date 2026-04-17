80e4c459085c1d66c2bf2cbacebb92bba7029625
Isaac Rodrigues <isaacrds2002@gmail.com>
2026-04-16 22:31:43 -0300

    feat: create class UsuarioRepository

---
+++ src/main/java/br/com/elementosearte/elementosearte_api/usuarios/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.elementosearte.elementosearte_api.usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
+
+}
