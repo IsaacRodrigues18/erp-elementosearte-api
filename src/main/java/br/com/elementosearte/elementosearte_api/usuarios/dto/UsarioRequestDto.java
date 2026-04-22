c90a0176c80c93b133913a0e97fdcb4425616294
Isaac Rodrigues <isaacrds2002@gmail.com>
2026-04-22 11:45:40 -0300

    feat: create class UsuarioRequestDto

---
+++ src/main/java/br/com/elementosearte/elementosearte_api/usuarios/dto/UsarioRequestDto.java
@@ -0,0 +1,37 @@
+package br.com.elementosearte.elementosearte_api.usuarios.dto;
+
+public class UsarioRequestDto {
+
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    public UsarioRequestDto() {
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
