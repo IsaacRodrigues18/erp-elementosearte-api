e1f9b7cac40cd9bcc9e21bbb1064a6d9621fa0fb
Isaac Rodrigues <isaacrds2002@gmail.com>
2026-04-22 21:34:02 -0300

    refactor: V2 migration add id_categoria

---
+++ src/main/java/br/com/elementosearte/elementosearte_api/categorias/CategoriaEntity.java
@@ -0,0 +1,39 @@
+package br.com.elementosearte.elementosearte_api.categorias;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_categorias")
+public class CategoriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long idCategoria;
+
+    @NotBlank
+    private String nome;
+
+    private String descricao;
+
+
+    private boolean ativo = true;
+
+    @Column(name = "criado_em", nullable = false)
+    private LocalDateTime criadoEm;
+
+    @Column(name = "atualizado_em", nullable = false)
+    private LocalDateTime atualizadoEm;
+}
--- src/main/resources/db/migration/V2__create_table_categoria.sql
+++ src/main/resources/db/migration/V2__create_table_categoria.sql
@@ -1,5 +1,5 @@
 create table tb_categorias (
-    id BIGSERIAL PRIMARY KEY,
+    id_categoria BIGSERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL UNIQUE,
     descricao VARCHAR(255),
     ativo BOOLEAN NOT NULL DEFAULT TRUE,
