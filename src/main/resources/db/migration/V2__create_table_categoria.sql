defaa76b02a40784e45430edb6671b6b229e97d5
Isaac Rodrigues <isaacrds2002@gmail.com>
2026-04-22 21:20:39 -0300

    feat: migration of categoria

---
+++ src/main/resources/db/migration/V2__create_table_categoria.sql
@@ -0,0 +1,10 @@
+create table tb_categorias (
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL UNIQUE,
+    descricao VARCHAR(255),
+    ativo BOOLEAN NOT NULL DEFAULT TRUE,
+    criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+
+);
