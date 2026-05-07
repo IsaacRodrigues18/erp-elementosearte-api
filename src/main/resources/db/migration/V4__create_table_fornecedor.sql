0fe6e7314c0e2e58df0927707000cbcd8029f67e
Isaac Rodrigues <isaacrds2002@gmail.com>
2026-05-07 17:19:13 -0300

    feat: migration V4__create_table_fornecedor

---
+++ src/main/resources/db/migration/V4__create_table_fornecedor.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE tb_fornecedores (
+
+                                 id_fornecedor BIGSERIAL PRIMARY KEY,
+
+                                 nome_fornecedor VARCHAR(255) NOT NULL,
+                                 cidade VARCHAR(255) NOT NULL,
+
+                                 telefone VARCHAR(20),
+                                 email VARCHAR(255) UNIQUE,
+
+                                 observacao VARCHAR(255),
+
+                                 ativo BOOLEAN NOT NULL DEFAULT TRUE,
+
+                                 criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                 atualizado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+);
