8f3309fe1a3d4c1b2e2e627b1a9080f2faea46ea
Isaac Rodrigues <isaacrds2002@gmail.com>
2026-06-10 21:30:53 -0300

    feat: migration movimentacao_estoque

---
+++ src/main/resources/db/migration/V7__create_table_movimentacaoEstoque.sql
@@ -0,0 +1,26 @@
+CREATE TABLE movimentacao_estoque
+(
+    id_movimentacao_estoque BIGSERIAL PRIMARY KEY,
+
+    id_produto BIGINT NOT NULL,
+
+    id_usuario BIGINT NOT NULL,
+
+    quantidade INTEGER NOT NULL,
+
+    valor_unitario DECIMAL(10,2),
+
+    tipo_movimentacao VARCHAR(30) NOT NULL,
+
+    observacao VARCHAR(255),
+
+    criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_movimentacao_produto
+        FOREIGN KEY (id_produto)
+            REFERENCES tb_produtos(id_produto),
+
+    CONSTRAINT fk_movimentacao_usuario
+        FOREIGN KEY (id_usuario)
+            REFERENCES tb_usuarios(id_usuario)
+);
